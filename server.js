0f99d8ca3628a384e1598ca885fd8db1d3dfa9e9
Suprathic <suprathickgs@gmail.com>
2026-03-07 15:51:45 +0530

    Added process.env.PORT

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const sqlite3 = require('sqlite3').verbose();
 const path = require('path'); // Pudhusa add pannirukkom
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
